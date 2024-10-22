9f77d993c47a9fcc05a925efe553872d955c6dc7
alpa26 <panovalecsander717@gmail.com>
2024-10-22 14:46:57 +0500

    Add Swagger UI

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
-  
+from rest_framework import viewsets
+from .models import TestModel
+from .serializers import TestModelSerializer
+
 def index(request):
     return HttpResponse("Добро пожаловать")
+
+class TestModelViewSet(viewsets.ModelViewSet):
+    queryset = TestModel.objects.all()
+    serializer_class = TestModelSerializer
