1826854d963b36a40ab8ddfc178fef7df8b30d7d
alpa26 <panovalecsander717@gmail.com>
2024-12-10 11:04:56 +0500

    update converter to pdf, models and files validation

--- backend/core/converters.py
+++ backend/core/converters.py
@@ -6,6 +6,41 @@ from pdf2image import convert_from_bytes
 from django.conf import settings
 from PyPDF2 import PdfReader, PdfWriter
 
+import re
+
+# Определите порядок элементов
+order = {
+    "birth_certificate": 1,
+    "statement": 2,
+    "marriage_certificate": 3,
+    "consent_to_pers_data_bank_employee": 4,
+    "consent_to_pers_data_minor": 5,
+    "cheque": 6,
+    "bank_reference": 7,
+    "cert_of_payment_med_services": 100
+}
+
+order_with_polis = {
+    "birth_certificate": 1,
+    "statement": 2,
+    "marriage_certificate": 3,
+    "consent_to_pers_data_bank_employee": 4,
+    "consent_to_pers_data_minor": 5,
+    "insurance_policy_VMI": 6,
+    "cert_about_paid_franchise_VMI": 7,
+    "cheque": 8,
+    "bank_reference": 9
+}
+
+def get_order_list(item, order):
+    for key in order:
+        if key in item:
+            suffix = re.findall(r'\d+', item)
+            suffix_value = int(suffix[-1]) if suffix else 0
+            # Корректируем порядок, если есть числовой суффикс
+            return (order[key] + suffix_value - 1, suffix_value)
+    return (float('inf'), 0)
+
 class FileConverter:
     """Класс для конвертации файлов в нужный формат."""
     def __init__(self, file=None, name=None):
@@ -35,7 +70,7 @@ class FileConverter:
         return [(ContentFile(temp_img.read()), filename)]
 
     def convert_images_to_pdf(self, session_id):
-        from core.models import Document
+        from core.models import MedicalInsurance, Document
 
         base_folder = os.path.join(settings.MEDIA_ROOT, 'backend/documents', session_id)
         output_pdf_path = os.path.join(base_folder, f'{session_id}_combined.pdf')
@@ -43,37 +78,38 @@ class FileConverter:
         if os.path.exists(output_pdf_path):
             return output_pdf_path  # Возвращаем существующий путь, если файл уже создан
 
-        image_files = []
-
-        pdf_files = []
-
+        _all_files = []
         for root, dirs, files in os.walk(base_folder):
             for file in files:
-                if file.endswith(".png"):
-                    image_files.append(os.path.join(root, file))
-                elif file.endswith(".pdf"):
-                    pdf_files.append(os.path.join(root, file))
+                if file.endswith(".png") or file.endswith(".pdf"):
+                    _all_files.append(os.path.join(root, file))
+
+        try:
+            medical_insurance = MedicalInsurance.objects.get(session_id=session_id)
+        except Exception as e:
+            medical_insurance = None
 
 
-        image_files.sort()
-        pdf_files.sort()
+        if medical_insurance and medical_insurance.is_policy_case :
+            all_files = sorted(_all_files, key=lambda x: get_order_list(x, order_with_polis))
+        else:
+            all_files = sorted(_all_files, key=lambda x: get_order_list(x, order))
 
-        # images = [Image.open(file).convert('RGB') for file in image_files]
         pdf_writer = PdfWriter()
 
-        for image_file in image_files:
-            image = Image.open(image_file).convert('RGB')
-            image_pdf = io.BytesIO()
-            image.save(image_pdf, format='PDF')
-            image_pdf.seek(0)
-            image_pdf_reader = PdfReader(image_pdf)
-            for page_num in range(len(image_pdf_reader.pages)):
-                pdf_writer.add_page(image_pdf_reader.pages[page_num])
-
-        for pdf_file in pdf_files:
-            with open(pdf_file, 'rb') as pdf:
-                pdf_reader = PdfReader(pdf)
-                pdf_writer.append_pages_from_reader(pdf_reader)
+        for file in all_files:
+            if file.endswith(".png"):
+                image = Image.open(file).convert('RGB')
+                image_pdf = io.BytesIO()
+                image.save(image_pdf, format='PDF')
+                image_pdf.seek(0)
+                image_pdf_reader = PdfReader(image_pdf)
+                for page_num in range(len(image_pdf_reader.pages)):
+                    pdf_writer.add_page(image_pdf_reader.pages[page_num])
+            else:
+                with open(file, 'rb') as pdf:
+                    pdf_reader = PdfReader(pdf)
+                    pdf_writer.append_pages_from_reader(pdf_reader)
 
         with open(output_pdf_path, 'wb') as output_pdf_file:
             pdf_writer.write(output_pdf_file)
@@ -90,6 +126,4 @@ class FileConverter:
         )
 
         return output_pdf_path
-
-
 
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,25 +1,22 @@
+import json
 import os
-
+import uuid
 from django.conf import settings
 from django.http import HttpResponse, JsonResponse
 from rest_framework import viewsets, status
 from rest_framework.parsers import MultiPartParser
-from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from core.models import Document
 from core.serializers import DocumentSerializer
 from core.converters import FileConverter
-
 from core.doc_services import DataInspector,remove_dir,check_is_file_exist_and_delete,delete_garbage_file
-
+from backend.transcriber import *
 
 
 def index(request):
     if not request.session.session_key:
         request.session.create()
     return HttpResponse("Добро пожаловать")
-
 
 class DocumentViewSet(viewsets.ModelViewSet):
     queryset = Document.objects.all()
@@ -39,35 +36,36 @@ class DocumentViewSet(viewsets.ModelViewSet):
         name, ext = os.path.splitext(request.FILES.get('path').name)
 
         self.perform_create(serializer,name, session_id)
+        auth_token = ''
 
         # Первое заявление
         statement1 = '''
         {
-          "Название документа": "Заявление",
+          "Название": "Заявление",
           "Дата": "2023-10-01",
-          "Подпись": true,
+          "Наличие подписи": true,
+          "Дата подписи": "2024-11-03",
           "ФИО заявителя": "Цветков Андрей Георгиевич",
           "ФИО ребенка": "Цветков Денис Андреевич",
-          "Дата рождения ребенка": "2011-12-03"
+          "ДР ребенка": "2011-12-03"
         }
         '''
-
         # Второе заявление
         statement2 = '''
         {
-          "Название документа": "Заявление",
+          "Название": "Заявление",
           "Дата": "2024-08-20",
-          "Подпись": false,
+          "Наличие подписи": false,
+          "Дата подписи": "2024-11-03",
           "ФИО заявителя": "Петров Сергей Александрович",
           "ФИО ребенка": "Петрова Анна Сергеевна",
-          "Дата рождения ребенка": "2024-08-20"
+          "ДР ребенка": "2024-08-20"
         }
         '''
-
         # Первое св. о браке
         marriage_certificate1 = '''
         {
-          "Название документа": "СВИДЕТЕЛЬСТВО О ЗАКЛЮЧЕНИИ БРАКА",
+          "Название": "СВИДЕТЕЛЬСТВО О ЗАКЛЮЧЕНИИ БРАКА",
           "ФИО мужа": "Цветков Андрей Георгиевич",
           "ФИО жены": "Цветкова Виктория Александровна"
         }
@@ -75,14 +73,13 @@ class DocumentViewSet(viewsets.ModelViewSet):
         # Первое св. о рождении
         birth_certificate1 = '''
         {
-          "Название документа": "СВИДЕТЕЛЬСТВО О РОЖДЕНИИ",
+          "Название": "СВИДЕТЕЛЬСТВО О РОЖДЕНИИ",
           "ФИО ребенка": "Цветков Денис Андреевич",
           "ДР ребенка": "03/12/2011",
           "ФИО отца": "Цветков Андрей Георгиевич",
           "ФИО матери": "Цветкова Виктория Александровна"
         }
         '''
-
         # Второе св. о браке
         marriage_certificate2 = '''
         {
@@ -91,7 +88,6 @@ class DocumentViewSet(viewsets.ModelViewSet):
           "ФИО жены": "Белова Александра Андреевна"
         }
         '''
-
         # второе св. о рождении
         birth_certificate2 = '''
         {
@@ -102,42 +98,303 @@ class DocumentViewSet(viewsets.ModelViewSet):
           "ФИО матери": "Белова Александра Андреевна"
         }
         '''
+        # чек
+        cheque1 ='''
+        {
+          "Название": "Чек",
+          "Способ оплаты": "Безналично",
+          "ФИО плательщика": "Цветков Андрей Георгиевич",
+          "Дата оплаты": "30/07/2024",
+          "Сумма": "16000.00",
+          "Место оплаты": "Больница 1",
+          "Подпись": true,
+          "Печать": true
+        }
+        '''
+        cheque2 ='''
+        {
+          "Название": "Чек",
+          "Способ оплаты": "Наличными",
+          "ФИО плательщика": "Цветков Андрей",
+          "Дата оплаты": "30/07/2024",
+          "Сумма": "16000.00",
+          "Место оплаты": "Больница 1",
+          "Подпись": true,
+          "Печать": false
+        }
+        '''
+        bank_reference1 ='''
+        {
+          "Название": "Выписка",
+          "ФИО плательщика": "Цветков Андрей Георгиевич",
+          "Дата оплаты": "30/07/2024",
+          "Сумма": "16000.00",
+          "Место оплаты": "Больница 1"
+        }
+        '''
+        bank_reference2 ='''
+        {
+          "Название": "Выписка",
+          "ФИО плательщика": "Цветков Иван Георгиевич",
+          "Дата оплаты": "30/05/2024",
+          "Сумма": "122000.00",
+          "Место оплаты": "Больница 2"
+        }
+        '''
+        cert_of_payment_med_services1 ='''
+        {
+          "Название": "Справка об оплате медицинских услуг",
+          "ФИО налогоплательщика": "Цветков Андрей Георгиевич",
+          "ДР налогоплательщика": "1988-12-03",
+          "Название организации": "Организация",
+          "Место оплаты": "Больница 1",
+          "ИНН": "111111111111",
+          "Паспортные данные": "1111 222222",
+          "Сумма расходов": "16000.00",
+          "ФИО выдавшего справку": "Иванов Иван Иванович",
+          "ФИО ребенка": "Цветков Денис Андреевич",
+          "ДР ребенка": "03/12/2011",
+          "Подпись": true,
+          "Печать": true
+        }
+        '''
+        insurance_policy_VMI1 ='''
+        {
+          "Название": "Полис ДМС",
+          "ФИО ребенка": "Цветков Денис Андреевич",
+          "ДР ребенка": "03/12/2011",
+          "Номер полиса": "4400 2888 9654 3821",
+          "Начало действия страхования": "25/01/2020",
+          "Окончание действия страхования": "25/01/2030"
+        }
+        '''
+        insurance_policy_VMI2 ='''
+        {
+          "Название": "Полис ДМС",
+          "ФИО ребенка": "Иванов Иван Иванович",
+          "ДР ребенка": "15/05/2010",
+          "Номер полиса": "1111 2888 9654 3821",
+          "Начало действия страхования": "25/01/2020",
+          "Окончание действия страхования": "25/01/2030"
+        }
+        '''
+        cert_about_paid_franchise_VMI1 ='''
+        {
+          "Название": "Справка от страховой компании",
+          "ФИО плательщика": "Цветков Андрей Георгиевич",
+          "ФИО ребенка": "Цветков Денис Андреевич",
+          "ДР ребенка": "03/12/2011",
+          "Номер полиса ДМС": "4400 2888 9654 3821",
+          "Дата начала страхования": "25/01/2020",
+          "Дата окончания страхования": "25/01/2030"
+        }
+        '''
+        cert_about_paid_franchise_VMI2 ='''
+        {
+          "Название": "Справка от страховой компании",
+          "ФИО плательщика": "Егоров Андрей Георгиевич",
+          "ФИО ребенка": "Иванов Иван Иванович",
+          "ДР ребенка": "15/05/2010",
+          "Номер полиса ДМС": "4400 2888 9654 3821",
+          "Дата начала страхования": "25/01/2020",
+          "Дата окончания страхования": "25/01/2023"
+        }
+        '''
+
+
         saved_instance = serializer.instance
 
-        # rquid = str(uuid.uuid4())
-        # auth_token = ''
-        #
-        # access_token = get_access_token(rquid, auth_token)
-        #
-        # img_path = saved_instance.path.name
-        # img_id = load_img(access_token, img_path)
-        # if("marriage_certificate" in saved_instance.name):
-        #     receipt_info1 = get_marriage_info(access_token, img_id)
-        #     print("1")
-        # elif ("birth_certificate" in saved_instance.name):
-        #     receipt_info2 = get_birth_info(access_token, img_id)
-        #     print("1")
+        rquid = str(uuid.uuid4())
 
-        headers = self.get_success_headers(serializer.data)
+        access_token = get_access_token(rquid, auth_token)
 
-        if("marriage_certificate" in saved_instance.name):
-            inspector = DataInspector(marriage_certificate1)
-            response= inspector.check_marriage_certificate(session_id)
+        img_path = saved_instance.path.name
+        if(ext == '.pdf'):
+            sourse_id = load_pdf(access_token, img_path)
+            info = get_info(access_token, sourse_id)
+        else:
+            sourse_id = load_img(access_token, img_path)
+
+        if ("marriage_certificate" in saved_instance.name):
+            if (ext == '.pdf'):
+                res = marriage_response(info, auth_token)
+            else:
+                res = process_marriage_certificate(access_token, sourse_id)
+
+            inspector = DataInspector(json.dumps(res))
+            response = inspector.check_marriage_certificate(session_id)
             if (response.status_code == 400):
                 delete_garbage_file(saved_instance.id)
                 return response
         elif ("statement" in saved_instance.name):
-            inspector = DataInspector(statement1)
-            response= inspector.check_statement(session_id)
-            if(response.status_code == 400):
-                delete_garbage_file(saved_instance.id)
-                return response
-        elif ("birth_certificate" in saved_instance.name):
-            inspector = DataInspector(birth_certificate1)
-            response= inspector.check_birth_certificate(session_id)
+            res = statement_response(info, auth_token)
+
+            inspector = DataInspector(json.dumps(res))
+            response = inspector.check_statement(session_id)
             if (response.status_code == 400):
                 delete_garbage_file(saved_instance.id)
                 return response
+        if ("birth_certificate" in saved_instance.name):
+            if (ext == '.pdf'):
+                res = birth_response(info, auth_token)
+            else:
+                res = process_birth_certificate(access_token, sourse_id)
+            inspector = DataInspector(json.dumps(res))
+            response = inspector.check_birth_certificate(session_id)
+            if (response.status_code == 400):
+                delete_garbage_file(saved_instance.id)
+                return response
+        elif ("cert_of_payment_med_services" in saved_instance.name):
+            res = double_page_response(info, auth_token)
+
+            inspector = DataInspector(json.dumps(res))
+            response = inspector.check_payment_reference(session_id)
+            if (response.status_code == 400):
+                delete_garbage_file(saved_instance.id)
+                return response
+        elif ("insurance_policy_VMI" in saved_instance.name):
+            if (ext == '.pdf'):
+                res = insurance_response(info, auth_token)
+            else:
+                res = process_insurance(access_token, sourse_id)
+
+            inspector = DataInspector(json.dumps(res))
+            response = inspector.check_policy(session_id)
+            if (response.status_code == 400):
+                delete_garbage_file(saved_instance.id)
+                return response
+        elif ("cert_about_paid_franchise_VMI" in saved_instance.name):
+            res = reference_six_response(info, auth_token)
+
+            inspector = DataInspector(json.dumps(res))
+            response = inspector.check_policy_reference(session_id)
+            if (response.status_code == 400):
+                delete_garbage_file(saved_instance.id)
+                return response
+        elif ("cheque" in saved_instance.name):
+            if (ext == '.pdf'):
+                res = receipt_response(info, auth_token)
+            else:
+                res = process_reciept(access_token, sourse_id)
+
+            inspector = DataInspector(json.dumps(res))
+            response = inspector.check_cheque(session_id)
+            if (response.status_code == 400):
+                delete_garbage_file(saved_instance.id)
+                return response
+        elif ("bank_reference" in saved_instance.name):
+            if (ext == '.pdf'):
+                res = reference_response(info, auth_token)
+            else:
+                res = process_reference(access_token, sourse_id)
+
+            inspector = DataInspector(json.dumps(res))
+            response = inspector.check_cheque_reference(session_id)
+            if (response.status_code == 400):
+                delete_garbage_file(saved_instance.id)
+                return response
+        print("Hell0")
+        # if ("marriage_certificate" in saved_instance.name):
+        #     # if (ext == '.pdf'):
+        #     #     res = marriage_response(info, auth_token)
+        #     # else:
+        #     #     res = process_marriage_certificate(access_token, sourse_id)
+        #     inspector = DataInspector(marriage_certificate1)
+        #     response = inspector.check_marriage_certificate(session_id)
+        #     if (response.status_code == 400):
+        #         delete_garbage_file(saved_instance.id)
+        #         return response
+        # elif ("statement" in saved_instance.name):
+        #     # res = statement_response(info, auth_token)
+        #
+        #     inspector = DataInspector(statement1)
+        #     response = inspector.check_statement(session_id)
+        #     if (response.status_code == 400):
+        #         delete_garbage_file(saved_instance.id)
+        #         return response
+        # if ("birth_certificate" in saved_instance.name):
+        #     # if (ext == '.pdf'):
+        #     #     res = birth_response(info, auth_token)
+        #     # else:
+        #     #     res = process_birth_certificate(access_token, sourse_id)
+        #
+        #     inspector = DataInspector(birth_certificate1)
+        #     response = inspector.check_birth_certificate(session_id)
+        #     if (response.status_code == 400):
+        #         delete_garbage_file(saved_instance.id)
+        #         return response
+        # elif ("cert_of_payment_med_services" in saved_instance.name):
+        #     #res = double_page_response(access_token, sourse_id)
+        #
+        #     inspector = DataInspector(cert_of_payment_med_services1)
+        #     response = inspector.check_payment_reference(session_id)
+        #     if (response.status_code == 400):
+        #         delete_garbage_file(saved_instance.id)
+        #         return response
+        # elif ("insurance_policy_VMI" in saved_instance.name):
+        #     # if (ext == '.pdf'):
+        #     #     res = insurance_response(info, auth_token)
+        #     # else:
+        #     #     res = process_insurance(access_token, sourse_id)
+        #
+        #     # res_max = sup_response(res, auth_token)
+        #
+        #     inspector = DataInspector(insurance_policy_VMI1)
+        #     response = inspector.check_policy(session_id)
+        #     if (response.status_code == 400):
+        #         delete_garbage_file(saved_instance.id)
+        #         return response
+        # elif ("cert_about_paid_franchise_VMI" in saved_instance.name):
+        #     # res = reference_six_response(access_token, sourse_id)
+        #
+        #     inspector = DataInspector(cert_about_paid_franchise_VMI1)
+        #     response = inspector.check_policy_reference(session_id)
+        #     if (response.status_code == 400):
+        #         delete_garbage_file(saved_instance.id)
+        #         return response
+        # elif ("cheque" in saved_instance.name):
+        #     # if (ext == '.pdf'):
+        #     #     res = receipt_response(info, auth_token)
+        #     # else:
+        #     #     res = process_reciept(access_token, sourse_id)
+        #
+        #     inspector = DataInspector(cheque1)
+        #     response = inspector.check_cheque(session_id)
+        #     if (response.status_code == 400):
+        #         delete_garbage_file(saved_instance.id)
+        #         return response
+        # elif ("bank_reference" in saved_instance.name):
+        #     # if (ext == '.pdf'):
+        #     #     res = reference_response(info, auth_token)
+        #     # else:
+        #     #     res = process_reference(access_token, sourse_id)
+        #
+        #     inspector = DataInspector(bank_reference1)
+        #     response = inspector.check_cheque_reference(session_id)
+        #     if (response.status_code == 400):
+        #         delete_garbage_file(saved_instance.id)
+        #         return response
+
+        headers = self.get_success_headers(serializer.data)
+
+        # if("marriage_certificate" in saved_instance.name):
+        #     inspector = DataInspector(marriage_certificate1)
+        #     response= inspector.check_marriage_certificate(session_id)
+        #     if (response.status_code == 400):
+        #         delete_garbage_file(saved_instance.id)
+        #         return response
+        # elif ("statement" in saved_instance.name):
+        #     inspector = DataInspector(statement1)
+        #     response= inspector.check_statement(session_id)
+        #     if(response.status_code == 400):
+        #         delete_garbage_file(saved_instance.id)
+        #         return response
+        # elif ("birth_certificate" in saved_instance.name):
+        #     inspector = DataInspector(birth_certificate1)
+        #     response= inspector.check_birth_certificate(session_id)
+        #     if (response.status_code == 400):
+        #         delete_garbage_file(saved_instance.id)
+        #         return response
 
         return JsonResponse(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
